8f4b953fd55a937392bf22c57d1dcc13e68808c0
William J. <wjanoti@users.noreply.github.com>
2026-08-15 20:37:11 -0300

    Clarify prevMap usage in two-integer-sum
    Added comment to clarify the use of prevMap in the algorithm.

--- Data Structures & Algorithms/two-integer-sum/submission-8.py
+++ Data Structures & Algorithms/two-integer-sum/submission-8.py
@@ -4,6 +4,7 @@ class Solution:
         for i in range(len(nums)):
             diff = target - nums[i]
             if diff in prevMap:
+                # prevMap only contains indexes smaller than i, so we need to use it first to satisfy the problem constraint.
                 return [prevMap[diff], i]
             prevMap[nums[i]] = i
         
