b582edc19f64eabb865eea4842dfe94a6e248b2e
William J. <wjanoti@users.noreply.github.com>
2026-08-15 20:41:01 -0300

    Clarify return condition with comment
    Add comment to clarify return condition in two-integer-sum.

--- Data Structures & Algorithms/two-integer-sum/submission-3.py
+++ Data Structures & Algorithms/two-integer-sum/submission-3.py
@@ -4,5 +4,6 @@ class Solution:
         for i, val in enumerate(nums):
             diff = target - val
             if diff in seen:
+                # seen only has indexes smaller than i, so it needs to be the first element in the return.
                 return[seen[diff], i]
             seen[val] = i
