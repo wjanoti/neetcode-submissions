4d80a1cc1ab3050bf1748bf3c663cb710facd773
William J. <wjanoti@users.noreply.github.com>
2026-08-20 01:04:16 -0300

    Add: python-stack-push-pop - submission-0

---
+++ Python Coding Interviews/python-stack-push-pop/submission-0.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    r = []
+    while arr:
+        r.append(arr.pop())
+    return r
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
